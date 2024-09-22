c572af1a91adc160050f0196a1f5ff590da97c41
Leonardo Bora <leonardo.bora@outlook.com>
2024-09-22 16:13:31 -0300

    adjustments

--- tests/test_robo_resgate.py
+++ tests/test_robo_resgate.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import unittest
 from unittest.mock import Mock
 from src.simulador_labirinto import SimuladorLabirinto
